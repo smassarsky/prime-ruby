3a5f5da88643da624375a4ae5a56bbe258a7868f
smassarsky <seth.massarsky@gmail.com>
2020-10-01 13:55:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num > 2
+    counter = 2
+    prime = true
+    while counter * counter <= num
+      if num % counter == 0
+        prime = false
+      end
+      counter += 1
+    end
+    prime
+  elsif num == 2
+    true
+  else
+    false
+  end
+end
